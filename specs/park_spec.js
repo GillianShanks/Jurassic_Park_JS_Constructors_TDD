5baebf6fcb0d9370491082237bb6986fcba827a0
Gillian Shanks <GillianShanks88@gmail.com>
2019-08-20 16:34:41 +0100

    Park constructor passes test for ticket price.

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -15,18 +15,21 @@ describe('Park', function() {
     assert.strictEqual(actual, 'Jurassic Park');
   });
 
-  xit('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.price;
+    assert.strictEqual(actual, 30);
+  });
 
-  xit('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs');
 
-  xit('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection');
 
-  xit('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection');
 
-  xit('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors');
 
-  xit('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species');
 
-  xit('should be able to remove all dinosaurs of a particular species');
+  it('should be able to remove all dinosaurs of a particular species');
 
 });
